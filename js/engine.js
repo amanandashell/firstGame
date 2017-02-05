a37aa090d5102f265c418629e9cf04b723e5f53b
Rami Charbel <amanandashell@gmail.com>
2017-02-05 17:54:07 +0100

    Add GameField rendering

---
+++ js/engine.js
@@ -0,0 +1,55 @@
+(function Engine(global){
+    var doc    = global.document,
+        win    = global.window,
+        canvas = doc.createElement("canvas"),
+        ctx    = canvas.getContext("2d"),
+        lastTime;
+    canvas.width  = 505;
+    canvas.height = 606;
+    doc.body.appendChild(canvas);
+
+
+    function main(){
+      var now = Date.now(),
+          dt  = (now - lastTime)/1000.0;
+
+      renderField();
+      lastTime = now;
+      win.requestAnimationFrame(main);
+    };
+
+    function init(){
+      lastTime = Date.now();
+      main();
+    };
+
+    function renderField(){
+      var rowImages =[
+            "images/water-block.png",
+            "images/stone-block.png",
+            "images/stone-block.png",
+            "images/stone-block.png",
+            "images/grass-block.png",
+            "images/grass-block.png"
+          ],
+          numRows = 6,
+          numCols = 5,
+          row, col;
+
+      for (row = 0; row < numRows; row++){
+        for (col = 0; col < numCols; col++){
+          ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+        }
+      }
+    };
+
+    Resources.onReady(init);
+    Resources.load([
+        "images/water-block.png",
+        "images/stone-block.png",
+        "images/stone-block.png",
+        "images/stone-block.png",
+        "images/grass-block.png",
+        "images/grass-block.png"
+      ]);
+})(this);
---
+++ js/resources.js
@@ -0,0 +1,49 @@
+(function Resources(window){
+  var loaded = [],
+      readyCallbacks = [];
+
+  function load(urlOrArr){
+    if(urlOrArr instanceof Array){
+      urlOrArr.forEach(function(url){
+        _load(url);
+      });
+    } else {
+      _load(urlOrArr);
+    }
+  };
+
+  function _load(url){
+    var img = new Image();
+    img.onload = function(){
+      loaded[url] = img;
+      if(_isReady()){
+        readyCallbacks.forEach(function(func){ func(); });
+      }
+    }
+    loaded[url] = false
+    img.src = url;
+  };
+
+  function get(url){
+    return loaded[url];
+  };
+
+  function _isReady(){
+    var ready = true;
+    for(k in loaded){
+      if(loaded.hasOwnProperty(k) && !loaded[k]) ready = false;
+    }
+    return ready;
+  };
+
+  function onReady(func){
+    readyCallbacks.push(func);
+  }
+
+  window.Resources = {
+    load: load,
+    get: get,
+    onReady: onReady
+  }
+
+})(this.window);
